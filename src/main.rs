454de5c8dd9bddbdba46e1445f17fab37d7dd315
VocalHorizon476 <arthur.thiele@dialysepraxis.com>
2026-05-05 09:18:51 +0200

    Switch from md to typ

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use std::env;
-use std::fs;
 use std::io;
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
 
-    println!("Reading folder: {}", path);
+    println!("Searching for typst files in: {}", path);
 
-    let entries = fs::read_dir(path)?;
+    let mut typst_files: Vec<PathBuf> = Vec::new();
 
-    for entry in entries {
+    for entry in WalkDir::new(path) {
         let entry = entry?;
-        println!("{}", entry.path().display());
+
+        if let Some(extension) = entry.path().extension() {
+            if extension == "typ" {
+                typst_files.push(entry.path().to_path_buf());
+            }
+        }
+    }
+
+    println!("Found {} typst files:", typst_files.len());
+    for path in &typst_files {
+        println!("  {}", path.display());
     }
 
     Ok(())
